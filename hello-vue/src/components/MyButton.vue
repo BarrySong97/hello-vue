f0bbd6e0274cc159910f4bd382aa320570d8d6ae
barrysong97 <524000659@qq.com>
2021-02-01 19:53:20 +0800

    test

---
+++ hello-vue/src/components/MyButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="handleClick">{{buttonName}}</button>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  methods: {
+    handleClick(event) {
+      this.$emit("move", event);
+    },
+  },
+})
+export default class Home extends Vue {
+    @Prop()
+    protected buttonName!: string;
+}
+</script>
---
+++ hello-vue/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" >
+      <my-button @move="handleClick" buttonName="emit"></my-button>
+      <h1 slot="nameSlot">name slot</h1>
+    </HelloWorld>
+  </div>
+</template>
+
+<script lang="ts">
+import MyButton from "@/components/MyButton.vue";
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+    MyButton
+  },
+  methods: {
+    handleClick(){
+      alert("emit success")
+      
+    }
+  }
+})
+export default class Home extends Vue {}
+</script>
